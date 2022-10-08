29d9d98f1a11b28f0f60c28b8fe8c7cfc8c7b4ad
Polina Shliakhina <98726255+codedbypolina@users.noreply.github.com>
2022-10-08 13:34:28 -0400

    Added kelvin project
    A mini-project to practice JS basics in Codecademy

---
+++ mini-projects/kelvin.js
@@ -0,0 +1,17 @@
+// Going over the basics of JS with Codecademy
+
+// Creating a constant variable
+const kelvin = 0;
+// Converting kelvin to celcius
+let celsius = kelvin - 273;
+// Converting celcius to fahrenheit
+let fahrenheit = celsius * (9 / 5) + 32;
+// Rounding down fahrenheit
+fahrenheit = Math.floor(fahrenheit);
+console.log("The temperature is " + fahrenheit + " degrees Fahrenheit");
+
+// Converting celsius to Newton
+let newton = celsius * (33 / 100);
+// Rounding down
+newton = Math.floor(newton);
+console.log(newton);
